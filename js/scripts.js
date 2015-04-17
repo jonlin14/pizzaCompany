92e6f5319799c231753bd601ed095cb56aaf1058
Jonathan Lin <johnlin0214@gmail.com>
2015-04-17 17:43:23 -0400

    Final styling changes

--- js/scripts.js
+++ js/scripts.js
@@ -20,63 +20,58 @@ var Pizza = {
             } // var pizza end
 
 $(function() {
-    var Order = { Total: 0, Cheese: 0, Pepperoni: 0 };
     $('form#new_Order').submit(function() {
+        event.preventDefault();
         $('.form1').hide();
         $('.form2').show();
-
-        event.preventDefault();
-       });
-
-
-
+    }); //end form new_Order
 
     $('form#pizza').submit(function() {
         event.preventDefault();
         var pizza_Choice = $('select#pizza_Choice').val();
         var pizza = Object.create(Pizza);
+        var amountOfPizzas = document.getElementById("pizza_Amount").selectedIndex;
         pizza.init();
         pizza.setName(pizza_Choice);
         pizza.setPrice();
-        var x = document.getElementById("pizza_Amount").selectedIndex;
-        var total = pizza.price * x;
-
+        var total = pizza.price * amountOfPizzas;
         $('#result').fadeIn();
         $('.price').text(total);
-
+        $('#subtotal').text('');
     }); //end form pizza
+
     $('#pizza_Choice').change(function() {
         var value = this.value;
-
-
-
         $('#result').hide();
         $('#images').text("");
+
         if (value === "Cheese") {
             $('#images').text("");
-            $('#images').append('<img id="cheese" class = "images" src="http://themommygamers.com/wp-content/uploads/8880327_orig.jpeg" />')
+            $('#images').append('<img id="cheese" class = "images" src="http://themommygamers.com/wp-content/uploads/8880327_orig.jpeg" />');
         }
+
         else if (value === "Pepperoni") {
             $('#images').text("");
-            $('#images').prepend('<img id ="pepperoni" class = "image" src="http://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/02/p41.jpg" />')
+            $('#images').prepend('<img id ="pepperoni" class = "image" src="http://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/02/p41.jpg" />');
         }
-        $('#pizza_Amount').change(function() {
 
-            var subtotal = 7.50 * document.getElementById("pizza_Amount").selectedIndex;
+        $('#pizza_Amount').change(function() {
+            $('#result').hide();
+            amountOfPizzas = document.getElementById("pizza_Amount").selectedIndex;
+            var subtotal = 7.50 * amountOfPizzas;
+
             if (value === "Cheese") {
             $('#images').text("");
-            $('#images').append('<img id="cheese" class = "images" src="http://themommygamers.com/wp-content/uploads/8880327_orig.jpeg" />')
-            $('#subtotal').html('<p> Your subtotal is $</p>' + subtotal);
+            $('#images').append('<img id="cheese" class = "images" src="http://themommygamers.com/wp-content/uploads/8880327_orig.jpeg" />');
+            $('#subtotal').html('<p> Your subtotal is $ ' + subtotal + ' for ' + amountOfPizzas + ' cheese pizzas </p>');
             }
-             else if (value === 'Pepperoni') {
-            subtotal = 10.00 * document.getElementById("pizza_Amount").selectedIndex;
+
+            else if (value === 'Pepperoni') {
+            subtotal = 10.00 * amountOfPizzas;
             $('#images').text("");
-            $('#images').prepend('<img id ="pepperoni" class = "image" src="http://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/02/p41.jpg" />')
-            $('#subtotal').html('<p> Your subtotal is $</p>' + subtotal);
+            $('#images').prepend('<img id ="pepperoni" class = "image" src="http://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/02/p41.jpg" />');
+            $('#subtotal').html('<p> Your subtotal is $ ' + subtotal + ' for ' + amountOfPizzas + ' pepperoni pizzas </p>');
             }
-    });
-    });
-     //#pizza_Choice select end
-
-
+        }); //end #pizza_Amount
+    }); //#pizza_Choice select end
 }); //function() end
