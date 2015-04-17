8f63694c9e06de1a36e2688e94c98486c5fa951c
Jonathan Lin <johnlin0214@gmail.com>
2015-04-17 15:32:27 -0400

    Add styling changes

--- js/scripts.js
+++ js/scripts.js
@@ -23,14 +23,30 @@ $(function() {
     $('form#pizza').submit(function() {
         event.preventDefault();
         var pizza_Choice = $('select#pizza_Choice').val();
-        console.log(pizza_Choice);
         var pizza = Object.create(Pizza);
         pizza.init();
         pizza.setName(pizza_Choice);
         pizza.setPrice();
 
+        $('#result').fadeIn();
         $('.price').text(pizza.price);
 
-    }); //form#pizza end
+    }); //end form pizza
+    $('#pizza_Choice').change(function() {
+        var value = this.value;
+        $('#result').hide();
+        $('#images').text("");
+        if (value === "Cheese")
+        {
+            $('#images').text("");
+            $('#images').append('<img id="cheese" class = "images" src="http://themommygamers.com/wp-content/uploads/8880327_orig.jpeg" />')
+        }
+        else if (value === 'Pepperoni') {
+            $('#images').text("");
+            $('#images').prepend('<img id ="pepperoni" class = "image" src="http://www.silviocicchi.com/pizzachef/wp-content/uploads/2015/02/p41.jpg" />')
+        }
+
+    });
+     //#pizza_Choice select end
 
 }); //function() end
