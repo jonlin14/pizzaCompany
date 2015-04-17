6d2eabe480e304d2435aea2d2305848afb2ddb13
Jonathan Lin <johnlin0214@gmail.com>
2015-04-17 13:54:29 -0400

    Completed basic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,27 @@ var Pizza = {
                 this.name = newName;
                 }, //setName end
                 setPrice: function() {
-                    if (this.name === 'cheese') {
+                    if (this.name === 'Cheese') {
                         this.price = 7.50;
                     }//if end
-                    else if (this.name === 'pepperoni') {
+                    else if (this.name === 'Pepperoni') {
                         this.price = 10.00;
-                    }//else end
+                    }//else if end
                 }//setPrice end
             } // var pizza end
+
+$(function() {
+    $('form#pizza').submit(function() {
+        event.preventDefault();
+        var pizza_Choice = $('select#pizza_Choice').val();
+        console.log(pizza_Choice);
+        var pizza = Object.create(Pizza);
+        pizza.init();
+        pizza.setName(pizza_Choice);
+        pizza.setPrice();
+
+        $('.price').text(pizza.price);
+
+    }); //form#pizza end
+
+}); //function() end
